fa731b8d4d5f9ec5a1f8d44db72af1835ca6b7ab
jmonagas <67768274+jmonagas@users.noreply.github.com>
2023-10-17 14:05:51 -0600

    Uploaded Full New Version of Website

---
+++ scripts/current_year.js
@@ -0,0 +1,1 @@
+const d=new Date;let year=d.getFullYear();document.getElementById("copyYear").innerHTML=year;
---
+++ scripts/nav_mobile.js
@@ -0,0 +1,1 @@
+function openNav(){document.getElementById("nav-lks").style.width="100vw"};function closeNav(){document.getElementById("nav-lks").style.width="0%"}
