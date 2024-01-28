73b121c134b5b13183fe3a141a861170fea0a3e6
jmonagas <67768274+jmonagas@users.noreply.github.com>
2024-01-27 17:42:03 -0700

    Revamped the entire website - for more SEO features and better Google ranking

--- scripts/current_year.js
+++
@@ -1,3 +0,0 @@
-const d = new Date;
-let year = d.getFullYear();
-document.getElementById("copyYear").innerHTML = year;
--- scripts/nav_mobile.js
+++
@@ -1,8 +0,0 @@
-function toggleLinks() {
-  let item = document.getElementById("nav-links");
-  if (item.style.display === "block") {
-    item.style.display = "none";
-  } else {
-    item.style.display = "block";
-  }
-}
