93b378ae7b2d753b7c6302a512bae151d1136fcc
jmonagas <67768274+jmonagas@users.noreply.github.com>
2024-05-01 18:05:16 -0600

    New Website Look

--- js/current_year.js
+++
@@ -1,3 +0,0 @@
-function openNav(){document.getElementById("top-nav").style.width="50%"};
-function closeNav(){document.getElementById("top-nav").style.width="0%"};
-const d=new Date;let year=d.getFullYear();document.getElementById("copyYear").innerHTML=year;
---
+++ js/main.js
@@ -0,0 +1,2 @@
+function showNav() { document.getElementById("nav-curtain").style.height = "100%" };
+function hideNav() { document.getElementById("nav-curtain").style.height = "0" };
