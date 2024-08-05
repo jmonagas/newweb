396cfcaac2b90953251ea8eacb0e65c0e927e7cf
jmonagas <67768274+jmonagas@users.noreply.github.com>
2024-08-05 09:41:16 -0600

    Brand New Design and Contents - Fully Recreated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-function showNav(){document.getElementById("nav-curtain").style.height="100%"};function hideNav(){document.getElementById("nav-curtain").style.height="0"}
+document.addEventListener("DOMContentLoaded", () => {
+	const e = document.getElementById("navToggle"),
+		t = document.getElementById("sideNav"),
+		d = document.getElementById("closeNav"),
+		n = () => {
+			"225px" === t.style.width ? (t.style.width = "0") : (t.style.width = "225px");
+		};
+	e.addEventListener("click", () => {
+		n();
+	}),
+		d.addEventListener("click", () => {
+			n();
+		}),
+		window.addEventListener("click", (n) => {
+			n.target === e || n.target === d || t.contains(n.target) || (t.style.width = "0");
+		});
+});
+(() => (document.getElementById("currentYear").textContent = new Date().getFullYear()))();
